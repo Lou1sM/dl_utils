cb1a03eda4aa59722f78631514916f4679391272
Lou1sM <oneillml@tcd.ie>
2022-01-14 16:27:17 +0000

    increase number of tests for digitify

--- src/misc_tests.py
+++ src/misc_tests.py
@@ -5,5 +5,5 @@ for i in range(100):
     for nd in [2,3,5,7]:
         assert int(n_digitify(i,nd)) == i
 
-for inp,nd in zip(np.random.randint(10,size=6),np.random.randint(4,size=6)):
+for inp,nd in zip(np.random.randint(10,size=30),np.random.randint(1,4,size=30)):
     print(f"{inp},{nd} --> {n_digitify(inp,nd)} for inp")
